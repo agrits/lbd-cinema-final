bd6776d57d38f71fb8231393345907519be0a1c6
Kacper Buczak <23583770+EnteerQ@users.noreply.github.com>
2019-08-19 15:36:42 +0200

    Create user related components. Add routing.
    Add componenets for '/login' , '/register' and routing for both.
    Create two modules:
     - user.module for user related components
     - cinema.module for cinema related components ( eg. movies, repertoire )

--- src/main/web/lbd-cinema-final/src/app/app.module.ts
+++ src/main/web/lbd-cinema-final/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { RouterModule } from "@angular/router";
+import { RegisterComponent } from "./user/register/register.component";
+import { UserModule } from "./user/user.module";
+import { LoginComponent } from "./user/login/login.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    UserModule,
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: "register", component: RegisterComponent },
+      { path: "login", component: LoginComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
