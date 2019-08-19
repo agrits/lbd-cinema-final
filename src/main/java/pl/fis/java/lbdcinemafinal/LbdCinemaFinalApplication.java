7ad157cdcb9a67489c002a275479eb96622b4b07
Artur Kasza <arturkasza98@gmail.com>
2019-08-19 12:19:56 +0200

    Initial commit.

---
+++ src/main/java/pl/fis/java/lbdcinemafinal/LbdCinemaFinalApplication.java
@@ -0,0 +1,13 @@
+package pl.fis.java.lbdcinemafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbdCinemaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbdCinemaFinalApplication.class, args);
+	}
+
+}
