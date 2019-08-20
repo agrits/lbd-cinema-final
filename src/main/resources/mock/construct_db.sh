ff28c212b0c4c1f5a6d9a782a4bdb0e3568f87ac
Artur Kasza <arturkasza98@gmail.com>
2019-08-20 15:19:10 +0200

    Update mock in new project structure.

--- src/main/resources/mock/construct_db.sh
+++ src/main/resources/mock/construct_db.sh
@@ -1,11 +1,12 @@
+echo "{" > db.json
 GLOBIGNORE="db.json"
 filenames=*.json
-echo "{" > db.json
 for filename in $filenames; do
   content="$(cat $filename)"
   filename="${filename%.*}"
   echo "\"$filename\": $content," >> "db.json"
 done
-echo -e "\r\b}" >> db.json
+
+echo "}" >> db.json
 
 unset GLOBIGNORE
