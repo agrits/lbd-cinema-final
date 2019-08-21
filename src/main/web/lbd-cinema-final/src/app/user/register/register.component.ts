7bc059f3230e42b32d9a6be9c6a345fd03cb2136
Kacper Buczak <23583770+EnteerQ@users.noreply.github.com>
2019-08-21 12:03:26 +0200

    Register component implemented.
    TODO: Send POST User request to DB with user information.

--- src/main/web/lbd-cinema-final/src/app/user/register/register.component.ts
+++ src/main/web/lbd-cinema-final/src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-register",
@@ -6,7 +7,54 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
+  private registerForm: FormGroup;
   constructor() {}
 
-  ngOnInit() {}
+  onSubmit() {
+    console.log(this.registerForm);
+  }
+
+  ngOnInit() {
+    this.initializeRegisterForm();
+  }
+  private initializeRegisterForm() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl(
+        "",
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.required,
+          Validators.maxLength(64)
+        ])
+      ),
+      lastName: new FormControl(
+        "",
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.required,
+          Validators.maxLength(64)
+        ])
+      ),
+      pesel: new FormControl(
+        "",
+        Validators.compose([
+          Validators.minLength(11),
+          Validators.required,
+          Validators.maxLength(11)
+        ])
+      ),
+      email: new FormControl(
+        "",
+        Validators.compose([Validators.required, Validators.email])
+      ),
+      password: new FormControl(
+        "",
+        Validators.compose([Validators.required, Validators.minLength(8)])
+      ),
+      confirmPassword: new FormControl(
+        "",
+        Validators.compose([Validators.required, Validators.minLength(8)])
+      )
+    });
+  }
 }
