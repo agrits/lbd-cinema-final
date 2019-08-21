04d7800e78b023e7dda4ace82b09c6a3b961e7c7
piotr.musiol <piotrek26697@gmail.com>
2019-08-21 09:38:06 +0200

    Update CustomEndpoint.java

--- cinema-service/src/main/java/pl/fis/java/cinemaservice/controllers/CustomEndpoint.java
+++ cinema-service/src/main/java/pl/fis/java/cinemaservice/controllers/CustomEndpoint.java
@@ -9,14 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import pl.fis.java.cinemaservice.repos.CustomLocationRepository;
+import pl.fis.java.cinemaservice.repos.ICustomLocationRepository;
 
 
 @RestController
 public class CustomEndpoint
 {
 	@Autowired
-	private CustomLocationRepository repo;
+	private ICustomLocationRepository repo;
 	
 	@RequestMapping(path = "/cities", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<String>> test()
