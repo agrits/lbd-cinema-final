88a3bf9f7824a906d4233582cb6702a5829a45ca
Wojciech Drzewiecki <wa.drzewiecki@gmail.com>
2019-08-23 09:13:06 +0200

    Migrate locations to hateoas

--- src/main/web/lbd-cinema-final/src/app/data-entity/localization/localization.ts
+++ src/main/web/lbd-cinema-final/src/app/data-entity/localization/localization.ts
@@ -1,23 +1,51 @@
-export interface LocalizationAttrs {
-    id: number;
-    city: string;
-    address: string;
-    lattitude: number;
-    longtitude: number;
-  }
-  export class Localization {
-    id: number;
-    city: string;
-    address: string;
-    lattitude: number;
-    longtitude: number;
+import { LinkHelper } from 'src/app/data-services/link-helper';
 
-    description: string;
-    constructor(attrs: Partial<LocalizationAttrs> = {}) {
-      this.id = attrs.id;
-      this.city = attrs.city
-      this.address = attrs.address;
-      this.lattitude = attrs.lattitude;
-      this.longtitude = attrs.longtitude;
-    }
+export interface GetLocalizationsResponse {
+  _embedded: {
+    locations: LocalizationAttrs[];
+  };
+}
+
+export interface LocalizationAttrs {
+  city: string;
+  address: string;
+  latitude: number;
+  longtitude: number;
+  _links: {
+    self: {
+      href: string;
+    };
+    location: {
+      href: string;
+    };
+    cinema: {
+      href: string;
+    };
   }
+}
+export class Localization {
+  id: Number;
+  city: string;
+  address: string;
+  latitude: number;
+  longtitude: number;
+  _links: {
+    self: {
+      href: string;
+    };
+    location: {
+      href: string;
+    };
+    cinema: {
+      href: string;
+    };
+  }
+
+  constructor(attrs: Partial<LocalizationAttrs>) {
+    this.id = LinkHelper.getIdFromSelfHref(attrs._links.self.href);
+    this.city = attrs.city
+    this.address = attrs.address;
+    this.latitude = attrs.latitude;
+    this.longtitude = attrs.longtitude;
+  }
+}
--- src/main/web/lbd-cinema-final/src/app/data-services/localization/localization.service.ts
+++ src/main/web/lbd-cinema-final/src/app/data-services/localization/localization.service.ts
@@ -1,44 +1,39 @@
 import { Injectable } from "@angular/core";
 import { DefaultService } from "../default.service";
-import { Localization, LocalizationAttrs } from "src/app/data-entity/localization/localization";
+import { Localization, LocalizationAttrs, GetLocalizationsResponse } from "src/app/data-entity/localization/localization";
 import { map } from "rxjs/operators";
 import { Observable } from 'rxjs';
 import { HttpParams } from '@angular/common/http';
-import { timeout } from 'rxjs/operators';
 
 @Injectable({
   providedIn: "root"
 })
 export class localizationService extends DefaultService {
-  
+
   protected localizationUrl: string = "http://localhost:8080/api/cinema/locations";
 
   getLocalizations(): Observable<Localization[]> {
     return this.httpClient
-    .get<Localization[]>(this.localizationUrl).pipe(timeout(20000));
-    /*.get<LocalizationAttrs[]>(this.localizationUrl)
+      .get<GetLocalizationsResponse>(this.localizationUrl)
       .pipe(
-        map(data => data.map(data => new Localization(data)))
-      ).pipe(timeout(20000));*/
-  }
-
-  updateLocalization(localization: Localization): Observable<LocalizationAttrs>{
-      return this.httpClient.put<LocalizationAttrs>(this.localizationUrl, localization);
+        map(embedded => embedded._embedded.locations.map(attrs => new Localization(attrs)))
+      );
   }
 
   getDefaultCity(longitude: string, latitude: string): Observable<Localization> {
     let params = new HttpParams().set("longitude", longitude).set("latitude", latitude);
-    return this.httpClient.get<Localization>(this.localizationUrl + "/default", {params: params}).pipe(timeout(90000));
+
+    return this.httpClient.get<LocalizationAttrs>(this.localizationUrl + "/default", { params: params })
+      .pipe(map(attrs => new Localization(attrs)));
   }
 
-  getPosition(): Promise<any>
-  {
+  getPosition(): Promise<any> {
     return new Promise((resolve, reject) => {
 
       navigator.geolocation.getCurrentPosition(resp => {
 
-          resolve({lng: resp.coords.longitude, lat: resp.coords.latitude});
-        },
+        resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
+      },
         err => {
           reject(err);
         });
